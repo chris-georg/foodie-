e89c48e71528da09447ab3a29adf9e7e3efa6bc9
chris-georg <chrisgeorge0425@gmail.com>
2025-12-04 17:12:45 +0100

    Update API URL to production endpoint

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from "./StoreContextContext";
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://foodie-backend-3p4k.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
